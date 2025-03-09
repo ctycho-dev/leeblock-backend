8b3446fdae7d766f947504b822d8599a95de008a
ctycho-dev <ctycho23@gmail.com>
2025-03-09 19:53:34 +0300

    typo fixed

--- app/main.py
+++ app/main.py
@@ -16,9 +16,9 @@ from app.routes import promo_code
 from app.routes import city
 from app.redis_client import redis_client
 from app.core.logger import get_logger
-from app.metrics import request_counter
-from app.metrics import response_counter
-from app.metrics import response_histogram
+# from app.metrics import request_counter
+# from app.metrics import response_counter
+# from app.metrics import response_histogram
 
 logger = get_logger()
 
@@ -41,33 +41,10 @@ app = FastAPI(lifespan=lifespan)
 logger.info('Start application')
 
 origins = [
-    "https://wwww.leeblock.ru",
+    "https://www.leeblock.ru",
     "http://localhost:3000",
     "http://localhost:3001",
 ]
-
-@app.middleware("http")
-async def dynamic_cors_middleware(request: Request, call_next):
-    start_time = time.time()
-    endpoint = request.url.path
-    
-    request_counter.inc({"path": endpoint})
-
-    response = await call_next(request)
-
-    elapsed_time = time.time() - start_time
-
-    response_histogram.observe({"path": endpoint}, elapsed_time)
-    response_counter.inc({"path": endpoint, "state": response.status_code})
-
-    # Check if the origin is allowed
-    # if origin in origins:
-    #     response.headers["Access-Control-Allow-Origin"] = origin
-    #     response.headers["Access-Control-Allow-Credentials"] = "true"
-    #     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    #     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-
-    return response
 
 app.add_middleware(
     CORSMiddleware,
@@ -86,3 +63,28 @@ app.include_router(root.router)
 app.include_router(user.router)
 app.include_router(promo_code.router)
 app.include_router(city.router)
+
+
+
+# @app.middleware("http")
+# async def dynamic_cors_middleware(request: Request, call_next):
+#     start_time = time.time()
+#     endpoint = request.url.path
+    
+#     request_counter.inc({"path": endpoint})
+
+#     response = await call_next(request)
+
+#     elapsed_time = time.time() - start_time
+
+#     response_histogram.observe({"path": endpoint}, elapsed_time)
+#     response_counter.inc({"path": endpoint, "state": response.status_code})
+
+#     # Check if the origin is allowed
+#     # if origin in origins:
+#     #     response.headers["Access-Control-Allow-Origin"] = origin
+#     #     response.headers["Access-Control-Allow-Credentials"] = "true"
+#     #     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+#     #     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+
+#     return response
